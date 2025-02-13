34a09058d59deb23bed652dce035e023bc00d41b
aravind <aravindaka14@gmail.com>
2025-02-13 02:51:57 -0800

    first commit

---
+++ src/components/DownloadPDF.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+import { PDFDownloadLink } from '@react-pdf/renderer'
+import { Invoice } from '../data/types'
+import InvoicePage from './InvoicePage'
+
+interface Props {
+  data: Invoice
+  setData(data: Invoice): void
+}
+
+const Download: FC<Props> = ({ data }) => {
+  const title = data.invoiceTitle ? data.invoiceTitle.toLowerCase() : 'invoice'
+
+  console.log(data)
+
+  return (
+    <PDFDownloadLink
+      document={<InvoicePage data={data} pdfMode={true} />}
+      fileName={`${title}.pdf`}
+      key={JSON.stringify(data)} // This ensures the component updates when `data` changes
+    >
+      {({ loading }) =>
+        loading ? 'Generating PDF...' : <button>Download Invoice</button>
+      }
+    </PDFDownloadLink>
+  )
+}
+
+export default Download
---
+++ src/data/initialData.ts
@@ -0,0 +1,50 @@
+import { ProductLine, Invoice } from './types'
+
+export const initialProductLine: ProductLine = {
+  description: '',
+  quantity: '',
+  rate: '',
+}
+
+export const initialInvoice: Invoice = {
+  logo: '',
+  logoWidth: 100,
+  title: 'INVOICE',
+  companyName: 'Ultrafly Solution',
+  name: '',
+  companyAddress: 'Puliyamkulam',
+  companyAddress2: 'Covi',
+  companyCountry: 'India State',
+  billTo: 'Bill To:',
+  clientName: '',
+  clientAddress: '',
+  clientAddress2: '',
+  clientCountry: 'india',
+  invoiceTitleLabel: 'Invoice#',
+  invoiceTitle: '',
+  invoiceDateLabel: 'Invoice Date',
+  invoiceDate: '',
+  invoiceDueDateLabel: 'Due Date',
+  invoiceDueDate: '',
+  productLineDescription: 'Item Description',
+  productLineQuantity: 'Qty',
+  productLineQuantityRate: 'Rate',
+  productLineQuantityAmount: 'Amount',
+  productLines: [
+    {
+      description: '',
+      quantity: '',
+      rate: '',
+    },
+    { ...initialProductLine },
+    { ...initialProductLine },
+  ],
+  subTotalLabel: 'Sub Total',
+  taxLabel: 'Sale Tax (10%)',
+  totalLabel: 'TOTAL',
+  currency: '$',
+  notesLabel: 'Notes',
+  notes: 'It was great doing business with you.',
+  termLabel: 'Terms & Conditions',
+  term: 'Please make the payment by the due date.',
+}
